4e14c6109d77fe32a9d6956065195bb4e681b40a
Tracy <tracy@Lucida-Bold.(none)>
2013-04-11 12:26:34 -0400

    now with internet!

--- scraper.py
+++ scraper.py
@@ -1,17 +1,19 @@
 from bs4 import BeautifulSoup
+import urllib
 
-page = BeautifulSoup(open("Detail.aspx.html"))
-print(page.title.string)
+def snag(url):
+    page = BeautifulSoup(urllib.urlopen(url).read())
+    print(page.title.string)
+    
+    name = page.find('h1', id='itemTitle')
+    print(name.string)
 
-name = page.find('h1', id='itemTitle')
-print(name.string)
-
-photo = page.find('img', id='imgPhoto', itemprop='image')
-print(photo['src'])
-
-ingred = []
-
-for span in page.find_all('span', 'ingredient-name'):
-    if span.has_key('class'): #and span['class'] == 'ingredient-name':
-        print(span.string)
-        ingred.append(span.string)
+    photo = page.find('img', id='imgPhoto', itemprop='image')
+    print(photo['src'])
+    
+    ingred = []
+    
+    for span in page.find_all('span', 'ingredient-name'):
+        if span.has_key('class'): #and span['class'] == 'ingredient-name':
+            print(span.string)
+            ingred.append(span.string)
